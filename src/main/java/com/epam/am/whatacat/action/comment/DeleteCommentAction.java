ba9d56e6c671f7517115949e6760bced22a72b1d
Alex Marushka <marushkaalex@gmail.com>
2016-10-18 18:51:01 +0600

    Refactor comment actions

--- src/main/java/com/epam/am/whatacat/action/comment/DeleteCommentAction.java
+++ src/main/java/com/epam/am/whatacat/action/comment/DeleteCommentAction.java
@@ -1,24 +1,24 @@
 package com.epam.am.whatacat.action.comment;
 
-import com.epam.am.whatacat.action.Action;
 import com.epam.am.whatacat.action.ActionException;
 import com.epam.am.whatacat.action.ActionResult;
+import com.epam.am.whatacat.action.BaseAction;
 import com.epam.am.whatacat.service.CommentService;
 import com.epam.am.whatacat.service.ServiceException;
-import com.epam.am.whatacat.utils.ParameterUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class DeleteCommentAction implements Action {
+public class DeleteCommentAction extends BaseAction {
+
     private static final Logger LOG = LoggerFactory.getLogger(DeleteCommentAction.class);
 
     @Override
-    public ActionResult execute(HttpServletRequest request, HttpServletResponse response) throws ActionException {
-        long id = ParameterUtils.parseLong(request.getParameter("id"), -1L);
-        if (id == -1) {
+    public ActionResult handle(HttpServletRequest request, HttpServletResponse response) throws ActionException {
+        long id = getIdParameter(request);
+        if (id == INVALID_ID) {
             return new ActionResult(HttpServletResponse.SC_BAD_REQUEST);
         }
 
--- src/main/java/com/epam/am/whatacat/action/comment/SendCommentAction.java
+++ src/main/java/com/epam/am/whatacat/action/comment/SendCommentAction.java
@@ -1,8 +1,8 @@
 package com.epam.am.whatacat.action.comment;
 
-import com.epam.am.whatacat.action.Action;
 import com.epam.am.whatacat.action.ActionException;
 import com.epam.am.whatacat.action.ActionResult;
+import com.epam.am.whatacat.action.BaseAction;
 import com.epam.am.whatacat.model.Comment;
 import com.epam.am.whatacat.model.User;
 import com.epam.am.whatacat.service.CommentService;
@@ -10,42 +10,57 @@ import com.epam.am.whatacat.service.ServiceException;
 import com.epam.am.whatacat.utils.ParameterUtils;
 import com.epam.am.whatacat.validation.FormValidator;
 import com.epam.am.whatacat.validation.FormValidatorFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Date;
-import java.util.List;
 import java.util.Map;
 
-public class SendCommentAction implements Action {
+public class SendCommentAction extends BaseAction {
+
+    private static final Logger LOG = LoggerFactory.getLogger(SendCommentAction.class);
+
+    private static final String VALIDATOR = "send-comment";
+    private static final String REDIRECT_URL = "post?id=";
+
+    private static final String PARAMETER_POST_ID = "post_id";
+    private static final String PARAMETER_PARENT_ID = "parent_id";
+    private static final String PARAMETER_TEXT = "text";
+
     @Override
-    public ActionResult execute(HttpServletRequest request, HttpServletResponse response) throws ActionException {
-        FormValidator validator = FormValidatorFactory.getInstance().getValidator("send-comment");
-        Map<String, String> errorMap = validator.validate(request.getParameterMap());
-        if (!errorMap.isEmpty()) {
-            request.getSession().setAttribute("errorMap", errorMap);
-            return new ActionResult("post?id=" + request.getParameter("post_id"), true);
+    public ActionResult handle(HttpServletRequest request, HttpServletResponse response) throws ActionException {
+        long postId = ParameterUtils.parseLong(request.getParameter(PARAMETER_POST_ID), INVALID_ID);
+        if (postId == INVALID_ID) {
+            return new ActionResult(HttpServletResponse.SC_BAD_REQUEST);
         }
 
-        User user = (User) request.getSession().getAttribute("user");
+        FormValidator validator = FormValidatorFactory.getInstance().getValidator(VALIDATOR);
+        Map<String, String> errorMap = validator.validate(request.getParameterMap());
+        if (!errorMap.isEmpty()) {
+            setErrorMap(request, errorMap);
+            return new ActionResult(REDIRECT_URL + postId, true);
+        }
+
+        User user = getUser(request);
         if (user == null) {
             return new ActionResult(HttpServletResponse.SC_UNAUTHORIZED);
-        }
-        long postId = ParameterUtils.parseLong(request.getParameter("post_id"), -1L);
-        if (postId == -1) {
-            return new ActionResult(HttpServletResponse.SC_BAD_REQUEST);
         }
 
         Comment comment = new Comment();
         comment.setAuthorId(user.getId());
-        comment.setParentId(ParameterUtils.parseLong(request.getParameter("parent_id"), null));
+        comment.setParentId(ParameterUtils.parseLong(request.getParameter(PARAMETER_PARENT_ID), null));
         comment.setPostId(postId);
-        comment.setText(request.getParameter("text"));
+        comment.setText(request.getParameter(PARAMETER_TEXT));
         comment.setPublicationDate(new Date());
 
         try (CommentService commentService = new CommentService()) {
             commentService.save(comment);
-            return new ActionResult("post?id=" + request.getParameter("post_id"), true);
+
+            LOG.info("{} has commented the post [{}]", user.getNickname(), postId);
+
+            return new ActionResult(REDIRECT_URL + postId, true);
         } catch (ServiceException e) {
             throw new ActionException(e);
         }
