6bce121354dfaff6d3bb7cbfb209e1c181ca035e
Alex Marushka <marushkaalex@gmail.com>
2016-10-24 00:05:16 +0600

    JdbcCommentDao column titles to constants

--- src/main/java/com/epam/am/whatacat/dao/jdbc/JdbcCommentDao.java
+++ src/main/java/com/epam/am/whatacat/dao/jdbc/JdbcCommentDao.java
@@ -19,6 +19,21 @@ public class JdbcCommentDao extends AbstractJdbcDao<Comment> implements CommentD
     private static final String TABLE_NAME = "comment";
     private static final String TABLE_NAME_USER = "user";
     private static final String TABLE_NAME_ROLE = "role";
+    private static final String COLUMN_ID = "id";
+    private static final String COLUMN_AUTHOR_ID = "author_id";
+    private static final String COLUMN_PARENT_ID = "parent_id";
+    private static final String COLUMN__DATE = "_date";
+    private static final String COLUMN_TEXT = "text";
+    private static final String COLUMN_POST_ID = "post_id";
+    private static final String COLUMN_EMAIL = "email";
+    private static final String COLUMN_NICKNAME = "nickname";
+    private static final String COLUMN_PASSWORD = "password";
+    private static final String COLUMN_ROLE_ID = "role_id";
+    private static final String COLUMN_GENDER = "gender";
+    private static final String COLUMN_RATING = "rating";
+    private static final String COLUMN_AVATAR = "avatar";
+    private static final String COLUMN_DATE = "date";
+    private static final String COLUMN_NAME = "name";
 
     private DataBinder<Comment> dataBinder = new CommentDataBinder();
 
@@ -39,36 +54,43 @@ public class JdbcCommentDao extends AbstractJdbcDao<Comment> implements CommentD
     @Override
     protected List<TableField> getTableFields() {
         return Arrays.asList(
-                new TableField(TABLE_NAME, "id"),
-                new TableField(TABLE_NAME, "author_id", "authorId"),
-                new TableField(TABLE_NAME, "parent_id", "parentId"),
-                new TableField(TABLE_NAME, "_date", "publicationDate").setTypeConverter(new DateTypeConverter()),
-                new TableField(TABLE_NAME, "text"),
-                new TableField(TABLE_NAME, "post_id", "postId"),
-                new TableField(TABLE_NAME_USER, "id").setUseOnSave(false),
-                new TableField(TABLE_NAME_USER, "email").setUseOnSave(false),
-                new TableField(TABLE_NAME_USER, "nickname").setUseOnSave(false),
-                new TableField(TABLE_NAME_USER, "password", "hashedPassword").setUseOnSave(false),
-                new TableField(TABLE_NAME_USER, "role_id", "role").setTypeConverter(o -> ((Role) o).getId()).setUseOnSave(false),
-                new TableField(TABLE_NAME_USER, "gender").setTypeConverter(o -> ((Gender) o).getKey()).setUseOnSave(false),
-                new TableField(TABLE_NAME_USER, "rating").setUseOnSave(false),
-                new TableField(TABLE_NAME_USER, "avatar", "avatarUrl").setUseOnSave(false),
-                new TableField(TABLE_NAME_USER, "date", "registrationDate").setTypeConverter(new DateTypeConverter()).setUseOnSave(false),
-                new TableField(TABLE_NAME_ROLE, "name").setUseOnSave(false)
+                new TableField(TABLE_NAME, COLUMN_ID),
+                new TableField(TABLE_NAME, COLUMN_AUTHOR_ID, "authorId"),
+                new TableField(TABLE_NAME, COLUMN_PARENT_ID, "parentId"),
+                new TableField(TABLE_NAME, COLUMN__DATE, "publicationDate").setTypeConverter(new DateTypeConverter()),
+                new TableField(TABLE_NAME, COLUMN_TEXT),
+                new TableField(TABLE_NAME, COLUMN_POST_ID, "postId"),
+                new TableField(TABLE_NAME_USER, COLUMN_ID).setUseOnSave(false),
+                new TableField(TABLE_NAME_USER, COLUMN_EMAIL).setUseOnSave(false),
+                new TableField(TABLE_NAME_USER, COLUMN_NICKNAME).setUseOnSave(false),
+                new TableField(TABLE_NAME_USER, COLUMN_PASSWORD, "hashedPassword").setUseOnSave(false),
+                new TableField(TABLE_NAME_USER, COLUMN_ROLE_ID, "role").setTypeConverter(o -> ((Role) o).getId()).setUseOnSave(false),
+                new TableField(TABLE_NAME_USER, COLUMN_GENDER).setTypeConverter(o -> ((Gender) o).getKey()).setUseOnSave(false),
+                new TableField(TABLE_NAME_USER, COLUMN_RATING).setUseOnSave(false),
+                new TableField(TABLE_NAME_USER, COLUMN_AVATAR, "avatarUrl").setUseOnSave(false),
+                new TableField(TABLE_NAME_USER, COLUMN_DATE, "registrationDate").setTypeConverter(new DateTypeConverter()).setUseOnSave(false),
+                new TableField(TABLE_NAME_ROLE, COLUMN_NAME).setUseOnSave(false)
         );
     }
 
     @Override
     protected String getJoin() {
-        return " JOIN user ON " + TABLE_NAME + ".author_id=" + TABLE_NAME_USER + ".id JOIN role ON "
-                + TABLE_NAME_ROLE + ".id=user.role_id";
+        return String.format(
+                " JOIN %s ON %s=%s JOIN %s ON %s=%s",
+                TABLE_NAME_USER,
+                makeTableField(TABLE_NAME, COLUMN_AUTHOR_ID),
+                makeTableField(TABLE_NAME_USER, COLUMN_ID),
+                TABLE_NAME_ROLE,
+                makeTableField(TABLE_NAME_ROLE, COLUMN_ID),
+                makeTableField(TABLE_NAME_USER, COLUMN_ROLE_ID)
+        );
     }
 
     @Override
     public List<Comment> getPostComments(long postId) throws DaoException {
         try (
                 PreparedStatement preparedStatement =
-                        getConnection().prepareStatement(getSelectQueryWithFrom() + getJoin() + " WHERE " + TABLE_NAME + ".post_id=?")
+                        getConnection().prepareStatement(getSelectQueryWithFrom() + getJoin() + " WHERE " + makeTableField(TABLE_NAME, COLUMN_POST_ID) + "=?")
         ) {
             preparedStatement.setLong(1, postId);
             ResultSet resultSet = preparedStatement.executeQuery();
@@ -85,22 +107,19 @@ public class JdbcCommentDao extends AbstractJdbcDao<Comment> implements CommentD
 
     @Override
     public void deletePostComments(long postId) throws DaoException {
-        try (PreparedStatement preparedStatement =
-                     getConnection().prepareStatement("DELETE FROM " + TABLE_NAME + " WHERE post_id=?")
-        ) {
-            preparedStatement.setLong(1, postId);
-            preparedStatement.execute();
-        } catch (SQLException e) {
-            throw new DaoException(e);
-        }
+        deleteCommentsByIdOfColumn(COLUMN_POST_ID, postId);
     }
 
     @Override
-    public void deleteUserComments(long postId) throws DaoException {
+    public void deleteUserComments(long authorId) throws DaoException {
+        deleteCommentsByIdOfColumn(COLUMN_AUTHOR_ID, authorId);
+    }
+
+    private void deleteCommentsByIdOfColumn(String columnName, long id) throws DaoException {
         try (PreparedStatement preparedStatement =
-                     getConnection().prepareStatement("DELETE FROM " + TABLE_NAME + " WHERE author_id=?")
+                     getConnection().prepareStatement("DELETE FROM " + TABLE_NAME + " WHERE " + columnName + "=?")
         ) {
-            preparedStatement.setLong(1, postId);
+            preparedStatement.setLong(1, id);
             preparedStatement.execute();
         } catch (SQLException e) {
             throw new DaoException(e);
